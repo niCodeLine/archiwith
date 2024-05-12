5999d2d40a2def47623a928f66dd3f97d69bee7a
Nico Spok <nicospok@hotmail.com>
2024-05-12 14:03:40 -0600

    Update archiwith.py

--- archiwith.py
+++ archiwith.py
@@ -2,10 +2,19 @@ from typing import TextIO, Callable
 
 class Archiwith:
     def __init__(self, file_path: str) -> Callable:
+        """
+        :param file_path: Path to your file.
+        """
         self.file_path: str = file_path
     
-    def abrir(self) -> TextIO:
-        self.file = open(self.file_path, 'r')
+    def abrir(self, binari: bool = False) -> TextIO:
+        """
+        :param binari: Set it to True if the file to be read is binary. Default False.
+        """
+        if binari:
+            self.file = open(self.file_path, 'rb')
+        else:
+            self.file = open(self.file_path, 'r')
         self._with = True
         return self.file
     
