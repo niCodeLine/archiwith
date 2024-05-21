13811689e221fa81025964aed8855dad43b96e8b
Nico Spok <nicospok@hotmail.com>
2024-05-21 00:22:24 -0600

    forgot to upload archiwith

---
+++ archiwith/archiwith.py
@@ -0,0 +1,27 @@
+from typing import TextIO, Callable
+
+class Archiwith:
+    def __init__(self, file_path: str) -> Callable:
+        self.file_path: str = file_path
+    
+    def abrir(self, binari: bool = False) -> TextIO:
+        if binari:
+            self.file = open(self.file_path, 'rb')
+        else:
+            self.file = open(self.file_path, 'r')
+        self._with = True
+        return self.file
+    
+    def escribir(self) -> TextIO:
+        self.file = open(self.file_path, 'w')
+        self._with = True
+        return self.file
+    
+    def leer(self) -> str:
+        self.file = open(self.file_path, 'r').read()
+        self._with = False
+        return self.file
+    
+    def __exit__(self):
+        if self._with:
+            self.file.close()
